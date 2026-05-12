2e7bfbfa84083f928a96dfbdaaf09c14373a1d18
titan_exasaur <tinytachyon14341@gmail.com>
2026-05-13 01:27:41 +0530

    setup complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document-copilot',
+    author='tiny_tachyon',
+    author_email='tinytachyon14341@gmail.com',
+    version='0.1.0',
+    packages=find_packages()
+)
--- template.sh
+++ template.sh
@@ -10,10 +10,12 @@ mkdir assets
 touch requirements.txt
 touch .gitignore
 
+touch src/__init__.py
 touch src/helper.py
 touch src/prompt.py
 
 touch app.py
+touch .env
 
 touch research/trials.ipynb
 touch setup.py
