6e97a0738b7e7a82c071b0f62d7df3e245da104d
titan_exasaur <tinytachyon14341@gmail.com>
2026-05-13 01:18:23 +0530

    project structure created

---
+++ template.sh
@@ -0,0 +1,19 @@
+# directories
+mkdir src
+mkdir research
+mkdir data
+mkdir assets
+
+
+
+# files
+touch requirements.txt
+touch .gitignore
+
+touch src/helper.py
+touch src/prompt.py
+
+touch app.py
+
+touch research/trials.ipynb
+touch setup.py
